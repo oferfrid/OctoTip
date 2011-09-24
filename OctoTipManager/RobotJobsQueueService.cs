7fb0e624f8d24d0e839b45dbc98e261b1be37655
Ofer Fridman <oferfrid@gmail.com>
2011-09-24 13:52:01 +0300

    oops missing files

---
+++ OctoTipManager/RobotJobsQueueService.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: oferfrid
+ * Date: 24/09/2011
+ * Time: 11:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+using OctoTip.OctoTipLib;
+
+namespace OctoTip.OctoTipManager
+{
+	
+	[ServiceContract]
+	public interface IRobotJobsQueueService
+	{
+		[OperationContract]
+		string SayHello(string name);
+	}
+
+
+	/// <summary>
+	/// Description of RobotJobsQueueService.
+	/// </summary>
+	public class RobotJobsQueueService:IRobotJobsQueueService
+	{
+		 LogString logger;
+		public RobotJobsQueueService()
+		{
+			     logger = LogString.GetLogString(OctoTipManager.MainForm.LOG_NAME);
+			      logger.Add("RobotJobsQueueService started");
+            
+		}
+		
+		public string SayHello(string name)
+		{
+			
+			
+			string response = string.Format("Hello, {0}", name);
+                logger.Add(response);
+			return string.Format(response);
+		}
+		
+	}
+}
