347bec8aaccf47c44fc9fe2f72c149ac0fbf9fa1
NQBTecan <Robot.NQB@gmail.com>
2011-10-06 19:19:23 +0200

    RobotWrapper.cs runs script, checks status in a loop, and raises event when status is changed
    Signed-off-by: NQBTecan <Robot.NQB@gmail.com>

--- OctoTipManager/RobotWrapper.cs
+++ OctoTipManager/RobotWrapper.cs
@@ -8,8 +8,9 @@
  */
 using System;
 using System.Configuration;
+using System.Diagnostics;
+using System.Threading;
 using EVOAPILib;
-
 using OctoTip.OctoTipLib;
 
 namespace OctoTip.OctoTipManager
@@ -20,15 +21,41 @@ namespace OctoTip.OctoTipManager
 	public class RobotWrapper
 	{
 		private EVOAPILib.System Evo;
+		int     ScriptID;
+		
 		public  const string LOG_NAME = "OctoTipManager";
 		private LogString myLogger = LogString.GetLogString(LOG_NAME);
+		
+		// Volatile is used as hint to the compiler that this data
+		// member will be accessed by multiple threads.
+		private volatile bool _ShouldStop = false;
+		private volatile bool _ShouldPause = false;
+		
 			
 		public RobotWrapper()
 		{
 			Evo = new EVOAPILib.SystemClass();
-//			Evo.StatusChanged +=  Evo_StatusChanged;
 		}
 		
+		public event EventHandler<RobotWrapperEventArgs> StatusChangeEvent;
+		
+		protected virtual void OnStatusChangeEvent(RobotWrapperEventArgs e)
+		{
+			EventHandler<RobotWrapperEventArgs> handler = StatusChangeEvent;
+
+			// Event will be null if there are no subscribers
+			if (handler != null)
+			{
+				// Use the () operator to raise the event.
+				handler(this, e);
+			}
+		}
+		
+
+
+		/// <summary>
+		/// Logon Freedom Evo
+		/// </summary>
 		private void Logon()
 		{			
 			SC_Status RobotStatus;
@@ -40,12 +67,10 @@ namespace OctoTip.OctoTipManager
 			{
 				Evo.Logon(UserName,Password,0,0);
 				RobotStatus = Evo.GetStatus();
-				Console.WriteLine(RobotStatus.ToString());
 				// initialazing
 				if (RobotStatus == SC_Status.STATUS_NOTINITIALIZED)
 				{
 					Evo.Initialize();
-					Console.WriteLine(RobotStatus.ToString());
 				}
 			}
 			catch(Exception e)
@@ -56,6 +81,9 @@ namespace OctoTip.OctoTipManager
 			}			
 		}
 		
+		/// <summary>
+		/// Logoff Freedom Evo
+		/// </summary>
 		private void Logoff()
 		{
 			try
@@ -70,61 +98,159 @@ namespace OctoTip.OctoTipManager
 			}
 		}
 		
-		public SC_ScriptStatus GetScriptStatus(int ScriptID)
+		/// <summary>
+		/// Runs in a loop to check script execution status.
+		/// Also checks if a pause or stop request arrived.
+		/// </summary>
+		private void CheckScriptStatus()
 		{
-			SC_ScriptStatus SS = Evo.GetScriptStatus(ScriptID);
-			return SS;
+			SC_ScriptStatus   ScriptStatusEx = SC_ScriptStatus.SS_UNKNOWN;
+			SC_ScriptStatus   ScriptStatus   = SC_ScriptStatus.SS_UNKNOWN;
+			ScriptTerminationStatusType  STS = ScriptTerminationStatusType.RuntimeError;
+			DateTime               StartTime = DateTime.Now;
+			TimeSpan                      TS = DateTime.Now - StartTime;
+
+			try
+			{				
+				ScriptStatusEx = Evo.GetScriptStatusEx(ScriptID);
+				ScriptStatus   = Evo.GetScriptStatus(ScriptID);
+				while(ScriptStatus == SC_ScriptStatus.SS_BUSY && !_ShouldStop)
+				{		
+					
+					System.Threading.Thread.Sleep(1000);
+					// Handeling pause request
+					if (_ShouldPause)
+					{
+						Evo.Pause();
+						while (_ShouldPause)
+						{
+							System.Threading.Thread.Sleep(1000);
+						}
+						Evo.Resume();
+					}
+					ScriptStatus = Evo.GetScriptStatus(ScriptID);
+				}				
+				
+				ScriptStatusEx = Evo.GetScriptStatusEx(ScriptID);
+				
+				// determain script termination status
+				switch (ScriptStatusEx)
+				{				
+					case SC_ScriptStatus.SS_IDLE:
+						STS = ScriptTerminationStatusType.Success;
+					break;
+					case SC_ScriptStatus.SS_BUSY:
+						STS = ScriptTerminationStatusType.Running;
+					break;	
+					case SC_ScriptStatus.SS_STOPPED:
+						STS = ScriptTerminationStatusType.TerminatedByUser;
+					break;
+					case SC_ScriptStatus.SS_ABORTED:
+						STS = ScriptTerminationStatusType.TerminatedByUser;
+					break;
+					case SC_ScriptStatus.SS_STATUS_ERROR:
+						STS = ScriptTerminationStatusType.Failed;
+					break;
+					default:
+						STS = ScriptTerminationStatusType.RuntimeError;
+					break;
+				}
+				
+				// Handling Stop request
+				if (_ShouldStop)
+				{
+					Evo.Stop();
+					STS = ScriptTerminationStatusType.TerminatedByUser;
+				}
+				
+			}
+			catch (Exception e)
+			{
+				myLogger.Add("Run Script Error");
+				myLogger.Add(e.ToString());
+				STS = ScriptTerminationStatusType.Failed;
+				
+				throw e;
+			}
+			finally
+			{
+				OnStatusChangeEvent(new RobotWrapperEventArgs(STS));
+			}
 		}
 		
-		public void RunScript(string ScriptName)
+		/// <summary>
+		/// Runs a script on the Robot.
+		/// </summary>
+		/// <param name="ScriptName">Name of script</param>
+		public void RunScript(object _ScriptName)
 		{		
-			SC_ScriptStatus ScriptStatus     = SC_ScriptStatus.SS_UNKNOWN;
-			SC_ScriptStatus PrevScriptStatus = SC_ScriptStatus.SS_UNKNOWN;
-			SC_Status		RobotStatus      = SC_Status.STATUS_UNKNOWN;
-			SC_Status		PrevRobotStatus  = SC_Status.STATUS_UNKNOWN;
+			string ScriptName =(string)_ScriptName;
+			ScriptTerminationStatusType STS = ScriptTerminationStatusType.Success;
+			
 			try
 			{
 				Logon();
-				Console.WriteLine("in run script");
-				int ScriptID = Evo.PrepareScript(ScriptName);
-				RobotStatus = Evo.GetStatus();
-				Console.WriteLine(RobotStatus.ToString());
+				ScriptID = Evo.PrepareScript(ScriptName);
 				Evo.StartScript(ScriptID, 0, 0);
-				DateTime StartTime = DateTime.Now;
-				TimeSpan TS = DateTime.Now - StartTime;
-
-				while(TS.TotalSeconds<60)
-				{
-					ScriptStatus = Evo.GetScriptStatus(ScriptID);
-					RobotStatus  = Evo.GetStatus();
-										
-					if ((ScriptStatus != PrevScriptStatus) || (RobotStatus != PrevRobotStatus))
-					{
-						Console.WriteLine(ScriptStatus.ToString());
-						Console.WriteLine(RobotStatus.ToString());
-						PrevScriptStatus = ScriptStatus;
-						PrevRobotStatus  = RobotStatus;
-					}
-					TS = (DateTime.Now - StartTime);					
-				}
-//				Console.WriteLine("Press any key to continue . . . ");
-//				Console.ReadKey(true);
+				CheckScriptStatus();
 			}
 			catch(Exception e)
 			{
 				myLogger.Add("Run Script Error");
 				myLogger.Add(e.ToString());
+				STS = ScriptTerminationStatusType.Failed;
+				OnStatusChangeEvent(new RobotWrapperEventArgs(STS));
+				throw e;
 			}
 			finally
 			{
 				Logoff();
 			}
 		}
+
+		public bool ShouldStop
+		{
+			get { return _ShouldStop;}
+		}
 		
-//		public void Evo_StatusChanged(object sender, EventArgs e)
-//		{
-//		 
-//		}
+		public bool ShouldPause
+		{
+			get { return _ShouldPause;}
+		}
+		
+		public void RequestPause()
+		{
+			_ShouldPause  = true;
+		}
+		
+		public void RequestResume()
+		{
+			_ShouldPause  = false;		
+		}		
+		
+		public void RequestStop()
+		{
+			_ShouldStop  = true;	
+						
+		}		
 	
+	}
+	
+	public enum ScriptTerminationStatusType
+	{Success=0, Running=1, RuntimeError=2, Failed=3, TerminatedByUser = 4};
+	
+	public class RobotWrapperEventArgs : EventArgs
+	{
+		private ScriptTerminationStatusType ScriptTerm;
+		public RobotWrapperEventArgs(ScriptTerminationStatusType _ScriptTerm)
+		{
+			ScriptTerm = _ScriptTerm;
+		}
+		
+		public ScriptTerminationStatusType ScriptTerminationStatus
+		{
+			get { return ScriptTerm; }
+			set { ScriptTerm = value;}
+		}
 	}
 }
