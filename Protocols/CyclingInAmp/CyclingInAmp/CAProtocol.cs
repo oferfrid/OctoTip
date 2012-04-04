13e334fedc10115452be3f6bbdf670a55934edb9
Ofer Fridman <oferfrid@gmail.com>
2012-04-04 18:05:26 +0300

    add CyclingInAmp

---
+++ Protocols/CyclingInAmp/CyclingInAmp/CAProtocol.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: oferfrid
+ * Date: 04/04/2012
+ * Time: 15:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using OctoTip.Lib.ExperimentsCore.Attributes;
+using OctoTip.Lib.ExperimentsCore.Base;
+
+namespace CyclingInAmp
+{
+	/// <summary>
+	/// Description of CAProtocol.
+	/// </summary>
+	[Protocol("Cyclic In AMP","Ofer Fridman","Cyclic Evolution In AMP with b-lac")]
+	public class CAProtocol:Protocol
+	{
+		public CAProtocol()
+		{
+		}
+		
+		protected override void DoWork()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
