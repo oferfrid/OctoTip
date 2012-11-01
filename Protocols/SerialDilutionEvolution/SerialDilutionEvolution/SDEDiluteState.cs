fbd341e62788beff91fedd23be5bdcf519bbef89
NQBTecan <Robot.NQB@gmail.com>
2012-11-01 16:09:29 +0200

    Protocol Are Working

---
+++ Protocols/SerialDilutionEvolution/SerialDilutionEvolution/SDEDiluteState.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tecan
+ * Date: 18/10/2012
+ * Time: 14:37
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using OctoTip.Lib.ExperimentsCore.Attributes;
+using OctoTip.Lib.ExperimentsCore.Base;
+
+namespace SerialDilutionEvolution
+{
+	/// <summary>
+	/// Description of SDEDiluteState.
+	/// </summary>
+	[State("Dilut","Dilute main evolution batch culture")]
+	 public class SDEDiluteState:State
+	{
+		public SDEDiluteState()
+		{
+		}
+	 	
+		protected override void DoWork()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Protocols/SerialDilutionEvolution/SerialDilutionEvolution/SDEProtocol.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tecan
+ * Date: 18/10/2012
+ * Time: 14:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using OctoTip.Lib.ExperimentsCore.Attributes;
+using OctoTip.Lib.ExperimentsCore.Base;
+
+namespace SerialDilutionEvolution
+{
+	/// <summary>
+	/// Description of SDEProtocol.
+	/// </summary>
+	[Protocol("Serial Dilution Evolution","Ofer Fridman","Serial Dilution Evolution experiment")]
+	public class SDEProtocol:Protocol
+	{
+		public SDEProtocol()
+		{
+		}
+		
+		protected override void DoWork()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
