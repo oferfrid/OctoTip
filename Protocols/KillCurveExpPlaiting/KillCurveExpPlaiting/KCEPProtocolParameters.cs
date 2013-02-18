7f5f380d2a4f4b6615e2a4f18aee1faf7fcd799a
Ofer Fridman <oferfrid@gmail.com>
2013-02-18 11:29:05 +0200

    Copy Of KillCurvePlaiting after rename

---
+++ Protocols/KillCurveExpPlaiting/KillCurveExpPlaiting/KCEPProtocolParameters.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: oferfrid
+ * Date: 06/05/2012
+ * Time: 12:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using OctoTip.Lib.ExperimentsCore.Attributes;
+using OctoTip.Lib.ExperimentsCore.Base;
+
+namespace KillCurveExpPlaiting
+{
+	/// <summary>
+	/// Description of KCEPProtocolParameters.
+	/// </summary>
+	public class KCEPProtocolParameters:ProtocolParameters
+	{		public KCEPProtocolParameters()
+		{
+		}	
+		[ProtocolParameterAtribute("Run Grow","true",true)]
+		public bool RunGrow;
+		[ProtocolParameterAtribute("Run Start","true",true)]
+		public bool RunStart;
+		[ProtocolParameterAtribute("Start eppendorf Index","1",true)]
+		public int SampleEppendorfInd;
+		[ProtocolParameterAtribute("Sample times (minutes)","10,15,20",true)]
+		public double[] SampleTimes;
+		[ProtocolParameterAtribute("Number Of Samples","1",true)]
+		public int NumberOfSamples;
+		[ProtocolParameterAtribute("Sample Position in Liconic","1",true)]
+		public int Sample6IndInLiconic;
+		[ProtocolParameterAtribute("ON Position in Liconic","2",true)]
+		public int ON96IndInLiconic;
+		[ProtocolParameterAtribute("ON start well index","1",true)]
+		public int ONStartwellIndex;
+		[ProtocolParameterAtribute("Start in sample","0",true)]
+		public int SampleIndex;
+		[ProtocolParameterAtribute("Hours 2 Grow 2 ON ","5",true)]
+		public double Hours2Grow2ON;
+		[ProtocolParameterAtribute("Log file path",@"D:\OctoTip\Protocols\KillCurveExpPlaiting\Output\")]
+		public string OutputFilePath;
+		[ProtocolParameterAtribute("AMP Posision in block","24",true)]
+		public int AMPPosision;
+		
+		
+		
+		
+	}
+}
---
+++ Protocols/KillCurveExpPlaiting/KillCurveExpPlaiting/KCEPSampleState.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: oferfrid
+ * Date: 07/05/2012
+ * Time: 09:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+using OctoTip.Lib;
+using OctoTip.Lib.ExperimentsCore.Attributes;
+using OctoTip.Lib.ExperimentsCore.Base;
+using OctoTip.Lib.ExperimentsCore.Interfaces;
+
+namespace KillCurveExpPlaiting
+{
+	/// <summary>
+	/// Description of KCEPSumpleState.
+	/// </summary>
+	[State("Sample","Sample bacteria to ")]
+	public class KCEPSampleState:RunRobotState,IRestartableState
+	{
+		
+		
+		int LicInd;
+		int SampleEppendorfInd;
+		int NumberOfSamples;
+		bool IsFirst;
+
+		
+		public KCEPSampleState(int LicInd,int SampleEppendorfInd,int NumberOfSamples,bool IsFirst):base()
+		{
+			this.LicInd = LicInd;
+			this.SampleEppendorfInd = SampleEppendorfInd;
+			this.NumberOfSamples =NumberOfSamples;
+			this.IsFirst = IsFirst;
+
+		}
+		
+		public void Restart()
+		{
+			this.Start();
+		}
+
+		protected override RobotJob BeforeRobotRun()
+		{
+			List<RobotJobParameter> RJP = new List<RobotJobParameter>(2);
+			
+			LicPos LP = Utils.Ind2LicPos(LicInd);
+
+///				@"ImportVariable(Liconic6PlateCart#Liconic6PlatePos#NumberOfSamples#FirstSampleInd#IsFirst,"D:\OctoTip\Protocols\KillCurveExpPlaiting\Scripts\TakeSampleData.csv",0#0#0#0#0,"1#1#2#1#0",0,1,0,1,1);",RJP);
+
+			RJP.Add(new RobotJobParameter("Liconic6PlateCart",RobotJobParameter.ParameterType.Number,LP.Cart));
+			RJP.Add(new RobotJobParameter("Liconic6PlatePos",RobotJobParameter.ParameterType.Number,LP.Pos));
+			RJP.Add(new RobotJobParameter("NumberOfSamples",RobotJobParameter.ParameterType.Number,NumberOfSamples));
+			RJP.Add(new RobotJobParameter("FirstSampleInd",RobotJobParameter.ParameterType.Number,SampleEppendorfInd));
+			RJP.Add(new RobotJobParameter("IsFirst",RobotJobParameter.ParameterType.Number, IsFirst ? 1 : 0));
+
+			
+			RobotJob RJ = new RobotJob(
+				@"D:\OctoTip\Protocols\KillCurveExpPlaiting\Scripts\TakeSample.esc",RJP);
+			
+			return RJ;
+		}
+		
+		protected override void AfterRobotRun()
+		{
+			
+		}
+		
+		
+		#region static
+		public static new List<Type> NextStates()
+		{
+			return new List<Type>{typeof(KCEPWaitState)};
+		}
+		#endregion
+	}
+}
